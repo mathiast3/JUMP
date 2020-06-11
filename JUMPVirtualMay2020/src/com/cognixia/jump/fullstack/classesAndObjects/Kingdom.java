833bddf527dd3357a176cb5f5dbb7f6d8b9cdbd5
Mathias <mathiasjtaylor@gmail.com>
2020-06-11 12:43:07 -0700

    deleted everything

--- JUMPVirtualMay2020/src/com/cognixia/jump/fullstack/classesAndObjects/Kingdom.java
+++
@@ -1,6 +0,0 @@
-package com.cognixia.jump.fullstack.classesAndObjects;
-
-public interface Kingdom {
-	public String getResp();
-	public void setResp(String resp);
-}
--- JUMPVirtualMay2020/src/com/cognixia/jump/fullstack/strings/StringDemo.java
+++
@@ -1,9 +0,0 @@
- package com.cognixia.jump.fullstack.strings;
-
-public class StringDemo {
-
-	public StringDemo() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
